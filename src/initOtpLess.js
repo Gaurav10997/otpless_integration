0d6336211e9d231acc11a4594851873dc2e9c66d
Gaurav10997 <avdhika@gmail.com>
2023-12-01 16:58:27 +0530

    completed

---
+++ src/initOtpLess.js
@@ -0,0 +1,26 @@
+export const initOTPless = () => {
+
+    var otplessInit = window[otplessInit];
+
+    const loadScript = () =>{
+        const script = document.createElement("script")
+        script.src = "https://otpless.com/auth.js";
+        script.id = "otplessIdScript";
+        document.body.appendChild(script)
+    }
+
+    if (otplessInit) {
+        otplessInit()
+    }
+    else {
+        loadScript()
+    }
+
+    window.otpless = (otplessUser) => {
+        const currentURL = window.location.href;
+        const urlWithoutParams = currentURL.split("?")[0];
+        window.history.pushState(null, "", urlWithoutParams);
+        alert("OTPLESS DATA :"+JSON.stringify(otplessUser))
+        window.location.href = '/dashboard';
+    };
+}
